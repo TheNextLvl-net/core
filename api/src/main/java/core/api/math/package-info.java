899b4514435ad33cb0ea619baf6930ec74ac4626
NonSwag <mrminecraft00@gmail.com>
2023-04-09 10:45:29 +0200

    deleted empty package

--- api/src/main/java/core/api/math/package-info.java
+++
@@ -1,9 +0,0 @@
-@FieldsAreNonnullByDefault
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package core.api.math;
-
-import core.annotation.FieldsAreNonnullByDefault;
-import core.annotation.MethodsReturnNonnullByDefault;
-import core.annotation.ParametersAreNonnullByDefault;
-
