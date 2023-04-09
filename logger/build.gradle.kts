3ed510e531ff321c9adbe6d25747365ff10a6129
NonSwag <mrminecraft00@gmail.com>
2023-04-09 10:14:16 +0200

    added publication config

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "net.thenextlvl.core"
@@ -16,4 +17,21 @@ dependencies {
     compileOnly(project(":annotations"))
 
     implementation(project(":api"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+        repositories {
+            maven {
+                url = uri("https://repo.thenextlvl.net/releases")
+                credentials {
+                    username = extra["RELEASES_USER"].toString()
+                    password = extra["RELEASES_PASSWORD"].toString()
+                }
+            }
+        }
+    }
 }
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "net.thenextlvl.core"
@@ -16,4 +17,21 @@ dependencies {
 
     annotationProcessor(project(":annotations"))
     annotationProcessor("org.projectlombok:lombok:1.18.26")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+        repositories {
+            maven {
+                url = uri("https://repo.thenextlvl.net/releases")
+                credentials {
+                    username = extra["RELEASES_USER"].toString()
+                    password = extra["RELEASES_PASSWORD"].toString()
+                }
+            }
+        }
+    }
 }
