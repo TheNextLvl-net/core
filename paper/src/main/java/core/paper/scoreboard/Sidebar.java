1d3625fd24bf48532ec42b6dac642202589cb5cf
NonSwag <mrminecraft00@gmail.com>
2023-05-21 18:52:03 +0200

    added sidebar api

---
+++ paper/src/main/java/core/paper/scoreboard/Sidebar.java
@@ -0,0 +1,93 @@
+package core.paper.scoreboard;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.*;
+
+public class Sidebar {
+    private final @Getter Player player;
+    private final Scoreboard scoreboard;
+    private final Objective objective;
+
+    public Sidebar(Player player) {
+        var scoreboard = player.getScoreboard();
+        var manager = Bukkit.getScoreboardManager();
+
+        if (scoreboard.equals(manager.getMainScoreboard()))
+            player.setScoreboard(scoreboard = manager.getNewScoreboard());
+
+        var objective = scoreboard.getObjective(DisplaySlot.SIDEBAR);
+        if (objective != null) objective.unregister();
+        objective = scoreboard.registerNewObjective("display", Criteria.DUMMY, Component.empty());
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+        this.player = player;
+        this.scoreboard = scoreboard;
+        this.objective = objective;
+    }
+
+    public Sidebar title(Component title) {
+        objective.displayName(title);
+        return this;
+    }
+
+    public Sidebar setScore(Component content, int score) {
+        getTeam(score).prefix(content);
+        return showScore(score);
+    }
+
+    public Sidebar unsetScore(int score) {
+        return hideScore(score);
+    }
+
+    private Sidebar showScore(int score) {
+        var value = Score.values()[score];
+        var objective = this.objective.getScore(value.color());
+        if (!objective.isScoreSet()) objective.setScore(score);
+        return this;
+    }
+
+    private Sidebar hideScore(int score) {
+        var value = Score.values()[score];
+        var objective = this.objective.getScore(value.color());
+        if (objective.isScoreSet()) scoreboard.resetScores(value.color());
+        return this;
+    }
+
+    private Team getTeam(int score) {
+        var value = Score.values()[score];
+        var team = scoreboard.getEntryTeam(value.name());
+        if (team != null) return team;
+        team = scoreboard.registerNewTeam(value.name());
+        team.addEntry(value.color());
+        return team;
+    }
+
+    @Getter
+    @Accessors(fluent = true)
+    @RequiredArgsConstructor
+    public enum Score {
+        SCORE_1("§1", 1),
+        SCORE_2("§2", 2),
+        SCORE_3("§3", 3),
+        SCORE_4("§4", 4),
+        SCORE_5("§5", 5),
+        SCORE_6("§6", 6),
+        SCORE_7("§7", 7),
+        SCORE_8("§8", 8),
+        SCORE_9("§9", 9),
+        SCORE_10("§0", 10),
+        SCORE_11("§a", 11),
+        SCORE_12("§b", 12),
+        SCORE_13("§c", 13),
+        SCORE_14("§d", 14),
+        SCORE_15("§e", 15);
+
+        private final String color;
+        private final int score;
+    }
+}
