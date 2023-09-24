3f25f992d7d1b103bdd09052dacc00fc1f0c31eb
NonSwag <mrminecraft00@gmail.com>
2023-09-24 13:23:49 +0200

    removed unused dependency

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,7 +9,7 @@ java {
 }
 
 group = "net.thenextlvl.core"
-version = "1.1.9-pre1"
+version = "1.1.9-pre2"
 
 repositories {
     maven("https://libraries.minecraft.net")
@@ -22,8 +22,6 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.28")
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
     compileOnly(project(":annotations"))
-
-    implementation(project(":api"))
 
     annotationProcessor("org.projectlombok:lombok:1.18.28")
 }
