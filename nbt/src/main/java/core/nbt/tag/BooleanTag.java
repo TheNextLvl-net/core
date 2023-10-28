afc4dd5ce2d46875225e5f00fbecd5191091bbd6
NonSwag <mrminecraft00@gmail.com>
2023-10-28 13:43:42 +0200

    added boolean tag

---
+++ nbt/src/main/java/core/nbt/tag/BooleanTag.java
@@ -0,0 +1,28 @@
+package core.nbt.tag;
+
+
+public class BooleanTag extends ByteTag {
+    public BooleanTag(boolean value) {
+        super((byte) (value ? 1 : 0));
+    }
+
+    public void setValue(boolean value) {
+        setValue((byte) (value ? 1 : 0));
+    }
+
+    @Override
+    public void setValue(Byte value) {
+        if (value == 0 || value == 1) super.setValue(value);
+        throw new IllegalArgumentException("value must be 0 or 1 but got: " + value);
+    }
+
+    @Override
+    public final boolean isBoolean() {
+        return true;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return getValue() == 1;
+    }
+}
