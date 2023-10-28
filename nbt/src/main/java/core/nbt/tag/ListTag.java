63b77b2ead85e3bac317e10fbdfdc23a48b6236b
NonSwag <mrminecraft00@gmail.com>
2023-10-28 13:57:50 +0200

    implement list

--- nbt/src/main/java/core/nbt/tag/ListTag.java
+++ nbt/src/main/java/core/nbt/tag/ListTag.java
@@ -1,21 +1,16 @@
 package core.nbt.tag;
 
+import core.nbt.NBTInputStream;
 import core.nbt.NBTOutputStream;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+import java.util.*;
 
 @Getter
-@Setter
-@ToString(callSuper = true)
-public class ListTag<V extends Tag> extends ValueTag<List<V>> {
+@EqualsAndHashCode(callSuper = true)
+public class ListTag<V extends Tag> extends ValueTag<List<V>> implements List<V> {
     public static final int ID = 9;
     private final int contentTypeId;
 
@@ -35,36 +30,7 @@ public class ListTag<V extends Tag> extends ValueTag<List<V>> {
     }
 
     public ListTag(int contentTypeId) {
-        super(new ArrayList<>());
-        this.contentTypeId = contentTypeId;
-    }
-
-    public V get(int index) {
-        return getValue().get(index);
-    }
-
-    public boolean contains(V v) {
-        return getValue().contains(v);
-    }
-
-    public boolean add(V v) {
-        return getValue().add(v);
-    }
-
-    public boolean remove(V v) {
-        return getValue().remove(v);
-    }
-
-    public int size() {
-        return getValue().size();
-    }
-
-    public boolean isEmpty() {
-        return getValue().isEmpty();
-    }
-
-    public void forEach(Consumer<? super V> action) {
-        getValue().forEach(action);
+        this(new ArrayList<>(), contentTypeId);
     }
 
     @Override
@@ -73,7 +39,128 @@ public class ListTag<V extends Tag> extends ValueTag<List<V>> {
     }
 
     @Override
-    public void write(@NotNull NBTOutputStream outputStream) throws IOException {
+    public int size() {
+        return getValue().size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return getValue().isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return getValue().contains(o);
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return getValue().iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return getValue().toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        return getValue().toArray(ts);
+    }
+
+    @Override
+    public boolean add(V v) {
+        return getValue().add(v);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return getValue().remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return new HashSet<>(getValue()).containsAll(collection);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends V> collection) {
+        return getValue().addAll(collection);
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends V> collection) {
+        return getValue().addAll(i, collection);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return getValue().removeAll(collection);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return getValue().retainAll(collection);
+    }
+
+    @Override
+    public void clear() {
+        getValue().clear();
+    }
+
+    @Override
+    public V get(int i) {
+        return getValue().get(i);
+    }
+
+    @Override
+    public V set(int i, V v) {
+        return getValue().set(i, v);
+    }
+
+    @Override
+    public void add(int i, V v) {
+        getValue().add(i, v);
+    }
+
+    @Override
+    public V remove(int i) {
+        return getValue().remove(i);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return getValue().indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return getValue().lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<V> listIterator() {
+        return getValue().listIterator();
+    }
+
+    @Override
+    public ListIterator<V> listIterator(int i) {
+        return getValue().listIterator(i);
+    }
+
+    @Override
+    public List<V> subList(int i, int i1) {
+        return getValue().subList(i, i1);
+    }
+
+    @Override
+    public String toString() {
+        return "ListTag{" +
+                "contentTypeId=" + contentTypeId +
+                ", value=" + super.getValue() +
+                '}';
+    }
+
     @Override
     public void write(NBTOutputStream outputStream) throws IOException {
         outputStream.writeByte(getContentTypeId());
