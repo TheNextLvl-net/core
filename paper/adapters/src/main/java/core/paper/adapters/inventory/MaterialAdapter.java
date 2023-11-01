4dfae2466bfa965966798102e77c4cac9036a18c
NonSwag <mrminecraft00@gmail.com>
2023-11-01 17:57:17 +0100

    split material adapter into two parts

--- paper/adapters/src/main/java/core/paper/adapters/inventory/MaterialAdapter.java
+++ paper/adapters/src/main/java/core/paper/adapters/inventory/MaterialAdapter.java
@@ -1,11 +1,11 @@
 package core.paper.adapters.inventory;
 
+import com.google.common.base.Preconditions;
 import com.google.gson.*;
 import core.paper.adapters.api.PaperAdapter;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.bukkit.Material;
-import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Type;
 
@@ -15,10 +15,35 @@ import java.lang.reflect.Type;
  * <i>This adapter is not required since {@link Material} is an enum, but it may be in the future</i>
  */
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class MaterialAdapter extends PaperAdapter<Material> {
 
-    @Override
-    public @Nullable Material deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
-        return Material.matchMaterial(element.getAsString());
+    /**
+     * This adapter deserializes non-existing or invalid materials into null
+     */
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Nullable extends MaterialAdapter {
+        public static final Nullable INSTANCE = new Nullable();
+
+        @Override
+        @org.jetbrains.annotations.Nullable
+        public Material deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+            return Material.matchMaterial(element.getAsString());
+        }
+    }
+
+    /**
+     * This adapter throws an exception when confronted with non-existing or invalid materials
+     */
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class NotNull extends MaterialAdapter {
+        public static final MaterialAdapter INSTANCE = new NotNull();
+
+        @Override
+        public Material deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+            var material = Material.matchMaterial(element.getAsString());
+            Preconditions.checkNotNull(material, "Invalid material: " + element.getAsString());
+            return material;
+        }
     }
 
     @Override
