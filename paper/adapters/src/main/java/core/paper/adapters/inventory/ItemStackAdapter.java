494cad79931c9b2eeecc2e19f7f4d3ebe7d645e6
NonSwag <mrminecraft00@gmail.com>
2023-11-01 16:09:36 +0100

    reworked the item adapter

--- paper/adapters/src/main/java/core/paper/adapters/inventory/ItemStackAdapter.java
+++ paper/adapters/src/main/java/core/paper/adapters/inventory/ItemStackAdapter.java
@@ -2,32 +2,32 @@ package core.paper.adapters.inventory;
 
 import com.google.gson.*;
 import core.paper.adapters.api.PaperAdapter;
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.lang.reflect.Type;
 
+/**
+ * This adapter uses a simple and short nbt format<br>
+ * <i>Example: minecraft:stone{display:{Name:'{"text":"example"}'}} 2</i>
+ */
 public class ItemStackAdapter extends PaperAdapter<ItemStack> {
     public static final ItemStackAdapter INSTANCE = new ItemStackAdapter();
 
     @Override
     public ItemStack deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
-        var object = element.getAsJsonObject();
-        var material = context.<Material>deserialize(object.get("type"), Material.class);
-        var meta = object.has("meta") ? context.<ItemMeta>deserialize(object.get("meta"), ItemMeta.class) : null;
-        var amount = object.get("amount").getAsInt();
-        var itemStack = new ItemStack(material, amount);
-        itemStack.setItemMeta(meta);
+        var string = element.getAsString();
+        var spaceIndex = string.lastIndexOf(' ');
+        var amount = Integer.parseInt(string.substring(spaceIndex + 1));
+        var itemStack = Bukkit.getItemFactory().createItemStack(string.substring(0, spaceIndex));
+        itemStack.setAmount(amount);
         return itemStack;
     }
 
     @Override
     public JsonElement serialize(ItemStack source, Type type, JsonSerializationContext context) {
-        var object = new JsonObject();
-        object.add("type", context.serialize(source.getType()));
-        if (source.hasItemMeta()) object.add("meta", context.serialize(source.getItemMeta()));
-        object.addProperty("amount", source.getAmount());
-        return object;
+        var material = source.getType().getKey().toString();
+        var meta = source.getItemMeta().getAsString();
+        return new JsonPrimitive(material + meta + " " + source.getAmount());
     }
 }
