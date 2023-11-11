69531aabbed24418837a305efcff05cde46a8bed
david <mrminecraft00@gmail.com>
2023-11-12 00:58:17 +0100

    optimized roman numeral generation

--- utils/src/main/java/core/util/StringUtil.java
+++ utils/src/main/java/core/util/StringUtil.java
@@ -1,15 +1,41 @@
 package core.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.Range;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class StringUtil {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class StringUtil {
 
     /**
      * The default characters for random strings
      */
     private static final char[] chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890".toCharArray();
+
+    /**
+     * The mapping between numbers and roman numerals
+     */
+    private static final Map<Integer, String> symbolMap = new HashMap<>(13);
+
+    static {
+        symbolMap.put(1000, "M");
+        symbolMap.put(900, "CM");
+        symbolMap.put(500, "D");
+        symbolMap.put(400, "CD");
+        symbolMap.put(100, "C");
+        symbolMap.put(90, "XC");
+        symbolMap.put(50, "L");
+        symbolMap.put(40, "XL");
+        symbolMap.put(10, "X");
+        symbolMap.put(9, "IX");
+        symbolMap.put(5, "V");
+        symbolMap.put(4, "IV");
+        symbolMap.put(1, "I");
+    }
 
     /**
      * Generate a random string
@@ -36,53 +62,19 @@ public class StringUtil {
     }
 
     /**
-     * Get an integer as a roman number
+     * Get an integer as a roman numeral
      *
      * @param input the integer
-     * @return the roman number
+     * @return the roman numeral
      */
     public static String roman(@Range(from = 1, to = 3999) int input) {
         var roman = new StringBuilder();
-        while (input > 0) {
-            if (input >= 1000) {
-                roman.append("M");
-                input -= 1000;
-            } else if (input >= 900) {
-                roman.append("CM");
-                input -= 900;
-            } else if (input >= 500) {
-                roman.append("D");
-                input -= 500;
-            } else if (input >= 400) {
-                roman.append("CD");
-                input -= 400;
-            } else if (input >= 100) {
-                roman.append("C");
-                input -= 100;
-            } else if (input >= 90) {
-                roman.append("XC");
-                input -= 90;
-            } else if (input >= 50) {
-                roman.append("L");
-                input -= 50;
-            } else if (input >= 40) {
-                roman.append("XL");
-                input -= 40;
-            } else if (input >= 10) {
-                roman.append("X");
-                input -= 10;
-            } else if (input == 9) {
-                roman.append("IX");
-                input -= 9;
-            } else if (input >= 5) {
-                roman.append("V");
-                input -= 5;
-            } else if (input == 4) {
-                roman.append("IV");
-                input -= 4;
-            } else {
-                roman.append("I");
-                input -= 1;
+        for (var entry : symbolMap.entrySet()) {
+            var value = entry.getKey();
+            var symbol = entry.getValue();
+            while (input >= value) {
+                roman.append(symbol);
+                input -= value;
             }
         }
         return roman.toString();
@@ -96,8 +88,8 @@ public class StringUtil {
      * @return whether the string is a palindrome
      */
     public static boolean isPalindrome(String string) {
-        StringBuilder reversed = new StringBuilder();
-        for (int index = string.length() - 1; index >= 0; index--) reversed.append(string.charAt(index));
+        var reversed = new StringBuilder();
+        for (var index = string.length() - 1; index >= 0; index--) reversed.append(string.charAt(index));
         return string.contentEquals(reversed);
     }
 }
