66c74b8d54b00425e9bf76943f08f47f6b15adae
david <mrminecraft00@gmail.com>
2024-04-13 11:40:31 +0200

    added worldless adapters

--- paper/adapters/build.gradle.kts
+++ paper/adapters/build.gradle.kts
@@ -11,7 +11,7 @@ java {
 }
 
 group = "net.thenextlvl.core"
-version = "1.0.8"
+version = "1.0.9"
 
 repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
--- paper/adapters/src/main/java/core/paper/adapters/world/LocationAdapter.java
+++ paper/adapters/src/main/java/core/paper/adapters/world/LocationAdapter.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import core.paper.adapters.api.PaperAdapter;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
 import org.bukkit.World;
 
@@ -46,6 +47,37 @@ public abstract class LocationAdapter extends PaperAdapter<Location> {
             object.addProperty("pitch", source.getPitch());
             return object;
         }
+
+        /**
+         * This adapter uses a more complex oop style with a predefined world<br>
+         */
+        @RequiredArgsConstructor
+        public static final class WorldLess extends LocationAdapter {
+            private final World world;
+
+            @Override
+            public Location deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+                var object = element.getAsJsonObject();
+                var x = object.get("x").getAsDouble();
+                var y = object.get("y").getAsDouble();
+                var z = object.get("z").getAsDouble();
+                var yaw = object.has("yaw") ? object.get("yaw").getAsFloat() : 0;
+                var pitch = object.has("pitch") ? object.get("pitch").getAsFloat() : 0;
+                return new Location(world, x, y, z, yaw, pitch);
+            }
+
+            @Override
+            public JsonElement serialize(Location source, Type type, JsonSerializationContext context) {
+                var object = new JsonObject();
+                object.addProperty("x", source.getX());
+                object.addProperty("y", source.getY());
+                object.addProperty("z", source.getZ());
+                if (source.getYaw() != 0 || source.getPitch() != 0) return object;
+                object.addProperty("yaw", source.getYaw());
+                object.addProperty("pitch", source.getPitch());
+                return object;
+            }
+        }
     }
 
     /**
@@ -79,6 +111,37 @@ public abstract class LocationAdapter extends PaperAdapter<Location> {
                 builder.append(", ").append(source.getYaw())
                         .append(", ").append(source.getPitch());
             return new JsonPrimitive(builder.toString());
+        }
+
+        /**
+         * This adapter uses a simple and short format with a predefined world<br>
+         * <i>Example: 0.5, 100, 0.5, 0, 90</i>
+         */
+        @RequiredArgsConstructor
+        public static final class WorldLess extends LocationAdapter {
+            private final World world;
+
+            @Override
+            public Location deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+                var split = element.getAsString().split(", ");
+                var x = Double.parseDouble(split[0]);
+                var y = Double.parseDouble(split[1]);
+                var z = Double.parseDouble(split[2]);
+                var yaw = split.length == 5 ? Float.parseFloat(split[3]) : 0;
+                var pitch = split.length == 5 ? Float.parseFloat(split[4]) : 0;
+                return new Location(world, x, y, z, yaw, pitch);
+            }
+
+            @Override
+            public JsonElement serialize(Location source, Type type, JsonSerializationContext context) {
+                var builder = new StringBuilder(String.valueOf(source.getX()))
+                        .append(", ").append(source.getY())
+                        .append(", ").append(source.getZ());
+                if (source.getYaw() != 0 || source.getPitch() != 0)
+                    builder.append(", ").append(source.getYaw())
+                            .append(", ").append(source.getPitch());
+                return new JsonPrimitive(builder.toString());
+            }
         }
     }
 }
