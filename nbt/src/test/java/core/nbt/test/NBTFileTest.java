3227049abf09d0e99e1ef027a6f2de352e3ec0a8
david <mrminecraft00@gmail.com>
2024-04-29 19:34:02 +0200

    added getOrDefault method

--- nbt/src/test/java/core/nbt/test/NBTFileTest.java
+++ nbt/src/test/java/core/nbt/test/NBTFileTest.java
@@ -5,14 +5,15 @@ import core.nbt.file.NBTFile;
 import core.nbt.tag.CompoundTag;
 import core.nbt.tag.DoubleTag;
 import core.nbt.tag.FloatTag;
+import core.nbt.tag.ListTag;
 
 public class NBTFileTest {
     public static void main(String[] args) {
         var file = new NBTFile<>(IO.of("player.dat"), new CompoundTag());
         var root = file.getRoot();
         root.forEach((s, tag) -> System.out.println(s + ": " + tag));
-        var pos = root.<DoubleTag>getAsList("Pos");
-        var rotation = root.<FloatTag>getAsList("Rotation");
+        var pos = root.getOrDefault("Pos", new ListTag<>(DoubleTag.ID));
+        var rotation = root.getOrDefault("Rotation", new ListTag<>(FloatTag.ID));
         pos.set(0, new DoubleTag(0d));
         pos.set(1, new DoubleTag(1d));
         pos.set(2, new DoubleTag(2d));
