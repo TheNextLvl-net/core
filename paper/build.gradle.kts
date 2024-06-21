942bdb5c7006d7e389bdf23c7ffd8e421250bf37
david <mrminecraft00@gmail.com>
2024-06-21 17:17:04 +0200

    added new methods added more javadocs

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -11,7 +11,7 @@ java {
 }
 
 group = "net.thenextlvl.core"
-version = "1.3.4"
+version = "1.3.5"
 
 repositories {
     mavenCentral()
--- paper/src/main/java/core/paper/item/ItemBuilder.java
+++ paper/src/main/java/core/paper/item/ItemBuilder.java
@@ -9,15 +9,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.inventory.meta.components.FoodComponent;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Range;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 
 @FieldsAreNotNullByDefault
@@ -28,6 +30,7 @@ public class ItemBuilder extends ItemStack {
      * Defaults stack size to 1, with no extra data
      *
      * @param type item material
+     * @see ItemStack#ItemStack(Material)
      */
     public ItemBuilder(Material type) {
         this(type, 1);
@@ -38,6 +41,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param type   item material
      * @param amount stack size
+     * @see ItemStack#ItemStack(Material, int)
      */
     public ItemBuilder(Material type, int amount) {
         this(new ItemStack(type, amount));
@@ -49,6 +53,7 @@ public class ItemBuilder extends ItemStack {
      * @param stack the stack to copy
      * @throws IllegalArgumentException if the specified stack is null, not an item or
      *                                  returns an item meta not created by the item factory
+     * @see ItemStack#ItemStack(ItemStack)
      */
     public ItemBuilder(ItemStack stack) throws IllegalArgumentException {
         super(stack);
@@ -58,6 +63,7 @@ public class ItemBuilder extends ItemStack {
      * Creates a new item builder with the provided player's head.
      *
      * @param player the player whose head to use
+     * @see #head(OfflinePlayer)
      */
     public ItemBuilder(OfflinePlayer player) {
         super(Material.PLAYER_HEAD);
@@ -65,13 +71,14 @@ public class ItemBuilder extends ItemStack {
     }
 
     /**
-     * Changes the display name of the item
+     * Changes the name of the item
      *
-     * @param name the new display name
+     * @param name the new item name
      * @return the modified item builder
+     * @see ItemMeta#itemName(Component)
      */
-    public ItemBuilder name(Component name) {
-        return modify(meta -> meta.displayName(name));
+    public ItemBuilder itemName(Component name) {
+        return modify(meta -> meta.itemName(name));
     }
 
     /**
@@ -79,9 +86,114 @@ public class ItemBuilder extends ItemStack {
      *
      * @param name the new display name
      * @return the modified item builder
+     * @see ItemMeta#displayName(Component)
      */
-    public ItemBuilder name(String name) {
-        return name(Component.text(name));
+    public ItemBuilder displayName(Component name) {
+        return modify(meta -> meta.displayName(name));
+    }
+
+    /**
+     * Sets the food component of the item.
+     *
+     * @param food the food component to set
+     * @return the modified item builder
+     * @see ItemMeta#setFood(FoodComponent)
+     */
+    @ApiStatus.Experimental
+    @SuppressWarnings("UnstableApiUsage")
+    public ItemBuilder food(@Nullable FoodComponent food) {
+        return modify(meta -> meta.setFood(food));
+    }
+
+    /**
+     * Changes the rarity of the item.
+     *
+     * @param rarity the new rarity of the item
+     * @return the modified item builder
+     * @see ItemMeta#setRarity(ItemRarity)
+     */
+    public ItemBuilder rarity(@Nullable ItemRarity rarity) {
+        return modify(meta -> meta.setRarity(rarity));
+    }
+
+    /**
+     * Sets the maximum stack size of the item.
+     *
+     * @param max the maximum stack size, or null to use the default stack size
+     * @return the modified ItemBuilder object
+     * @see ItemMeta#setMaxStackSize(Integer)
+     */
+    public ItemBuilder maxStackSize(@Range(from = 1, to = 99) @Nullable Integer max) {
+        return modify(meta -> meta.setMaxStackSize(max));
+    }
+
+    /**
+     * Sets the amount of the item.
+     *
+     * @param amount the amount to set
+     * @return the modified item builder
+     * @see ItemStack#setAmount(int)
+     */
+    public ItemBuilder amount(@Range(from = 0, to = 99) int amount) {
+        setAmount(Math.clamp(amount, 0, getMaxStackSize()));
+        return this;
+    }
+
+    /**
+     * Increases the amount of the item by the specified value.
+     *
+     * @param amount the amount to add
+     * @return the modified item builder
+     * @see ItemStack#add(int)
+     * @see #amount(int)
+     */
+    public ItemBuilder addAmount(int amount) {
+        return amount(getAmount() + amount);
+    }
+
+    /**
+     * Decreases the amount of the item by the specified value.
+     *
+     * @param amount the amount to subtract
+     * @return the modified item builder
+     * @see ItemStack#subtract(int)
+     * @see #amount(int)
+     */
+    public ItemBuilder subtractAmount(int amount) {
+        return amount(getAmount() - amount);
+    }
+
+    /**
+     * Makes this item unbreakable.
+     *
+     * @param unbreakable true if the item should be unbreakable, false otherwise
+     * @return the modified item builder
+     * @see ItemMeta#setUnbreakable(boolean)
+     */
+    public ItemBuilder unbreakable(boolean unbreakable) {
+        return modify(meta -> meta.setUnbreakable(unbreakable));
+    }
+
+    /**
+     * Modifies the custom model data of the item.
+     *
+     * @param data the custom model data value to set
+     * @return the modified item builder
+     * @see ItemMeta#setCustomModelData(Integer)
+     */
+    public ItemBuilder customModelData(@Nullable Integer data) {
+        return modify(meta -> meta.setCustomModelData(data));
+    }
+
+    /**
+     * Sets the enchantment glint of the item.
+     *
+     * @param override true to make it glint, false to hide it, null to reset
+     * @return the modified item builder
+     * @see ItemMeta#setEnchantmentGlintOverride(Boolean)
+     */
+    public ItemBuilder enchantmentGlintOverride(@Nullable Boolean override) {
+        return modify(meta -> meta.setEnchantmentGlintOverride(override));
     }
 
     /**
@@ -89,6 +201,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param tooltip true to hide the tooltip, false to show it
      * @return the modified item builder
+     * @see ItemMeta#setHideTooltip(boolean)
      */
     public ItemBuilder hideTooltip(boolean tooltip) {
         return modify(meta -> meta.setHideTooltip(tooltip));
@@ -100,6 +213,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param lore the lore that will be set
      * @return the modified item builder
+     * @see ItemMeta#lore(List)
      */
     public ItemBuilder lore(Component... lore) {
         return modify(meta -> {
@@ -109,27 +223,12 @@ public class ItemBuilder extends ItemStack {
     }
 
     /**
-     * Changes the lore of the item
-     * Removes lore when given an empty text
-     * Only for textblocks
-     *
-     * @param text the textblock that will be set as lore
-     * @return the modified item builder
-     */
-    public ItemBuilder lore(String text) {
-        var lines = text.split("\n");
-        return lore(Arrays.stream(lines)
-                .map(Component::text)
-                .toList()
-                .toArray(new Component[]{})
-        );
-    }
-
-    /**
      * Appends to the lore of the item
      *
      * @param lore the lore that will be appended
      * @return the modified item builder
+     * @see ItemMeta#lore(List)
+     * @see #lore(Component...)
      */
     public ItemBuilder appendLore(Component... lore) {
         return modify(meta -> {
@@ -141,26 +240,11 @@ public class ItemBuilder extends ItemStack {
     }
 
     /**
-     * Appends to the lore of the item
-     * Only for textblocks
-     *
-     * @param text the textblock that will be appended
-     * @return the modified item builder
-     */
-    public ItemBuilder appendLore(String text) {
-        var lines = text.split("\n");
-        return appendLore(Arrays.stream(lines)
-                .map(Component::text)
-                .toList()
-                .toArray(new Component[]{})
-        );
-    }
-
-    /**
      * Set item flags which should be ignored when rendering an ItemStack.
      *
      * @param itemFlags The flags which shouldn't be rendered
      * @return the modified item builder
+     * @see ItemMeta#addItemFlags(ItemFlag...)
      */
     public ItemBuilder itemFlags(ItemFlag... itemFlags) {
         return modify(meta -> meta.addItemFlags(itemFlags));
@@ -171,6 +255,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param player the player to set
      * @return the modified item builder
+     * @see #head(PlayerProfile)
      */
     public ItemBuilder head(OfflinePlayer player) {
         return head(player.getPlayerProfile());
@@ -181,6 +266,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param profile the profile to set
      * @return the modified item builder
+     * @see SkullMeta#setPlayerProfile(PlayerProfile)
      */
     public ItemBuilder head(PlayerProfile profile) {
         return modify(SkullMeta.class, meta -> meta.setPlayerProfile(profile));
@@ -188,9 +274,13 @@ public class ItemBuilder extends ItemStack {
 
     /**
      * Defines the owner of the skull meta
+     * <p>
+     * Note: This is a blocking action
      *
      * @param player the player to set
      * @return the modified item builder
+     * @see #head(OfflinePlayer)
+     * @see Bukkit#getOfflinePlayer(String)
      */
     public ItemBuilder head(String player) {
         return head(Bukkit.getOfflinePlayer(player));
@@ -201,6 +291,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param base64 the head value to set
      * @return the modified item builder
+     * @see #head(PlayerProfile)
      */
     public ItemBuilder headValue(String base64) {
         var id = new UUID(base64.hashCode(), base64.hashCode());
@@ -214,6 +305,7 @@ public class ItemBuilder extends ItemStack {
      *
      * @param url the url to set
      * @return the modified item builder
+     * @see #headValue(String)
      */
     public ItemBuilder headURL(String url) {
         var texture = "{\"textures\":{\"SKIN\":{\"url\":\"" + url + "\"}}}";
@@ -226,7 +318,8 @@ public class ItemBuilder extends ItemStack {
      *
      * @param consumer the meta consumer
      * @return the modified item builder
-     * @see ItemBuilder#modify(Class, Consumer)
+     * @see ItemStack#editMeta(Consumer)
+     * @see #modify(Class, Consumer)
      */
     public ItemBuilder modify(Consumer<? super ItemMeta> consumer) {
         return modify(ItemMeta.class, consumer);
@@ -239,6 +332,7 @@ public class ItemBuilder extends ItemStack {
      * @param consumer  the meta consumer
      * @param <M>       the meta type
      * @return the modified item builder
+     * @see ItemStack#editMeta(Class, Consumer)
      */
     public <M extends ItemMeta> ItemBuilder modify(Class<M> metaClass, Consumer<? super M> consumer) {
         editMeta(metaClass, consumer);
