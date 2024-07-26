a1bb30645af88df8131da9162ae75625417537a2
david <mrminecraft00@gmail.com>
2024-07-26 20:18:57 +0200

    Add java-library plugin, JitPack repo, and dependencies

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("java-library")
     id("maven-publish")
 }
 
@@ -15,6 +16,7 @@ version = "1.4.0"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
@@ -23,6 +25,9 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.34")
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     compileOnly(project(":annotations"))
+
+    api(project(":version-checker"))
+    api("com.github.xmrafonso:hangar4j:1.2.2")
 
     annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
