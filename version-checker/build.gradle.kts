e215b5cee1516c872265a18f89865bebba9f01af
david <mrminecraft00@gmail.com>
2024-12-08 17:36:00 +0100

    Update Lombok version to 1.18.36 across modules
    Upgrade the Lombok library from version 1.18.34 to 1.18.36 in all build.gradle.kts files to ensure consistency across all modules. This update may include bug fixes or enhancements, improving overall compatibility and performance. Additionally, update the Paper API version to 1.21.4 in relevant modules and remove outdated maven repository entries for better dependency management.

--- version-checker/build.gradle.kts
+++ version-checker/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:1.18.36")
 
     api("com.google.code.gson:gson:2.11.0")
     compileOnly("org.jspecify:jspecify:1.0.0")
@@ -32,7 +32,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
 tasks.test {
