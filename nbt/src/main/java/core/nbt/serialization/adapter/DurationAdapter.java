6428cffe5a487f4e4f718341956c047bf167c45e
david <mrminecraft00@gmail.com>
2025-02-01 09:11:31 +0100

    Add support for serializing and deserializing Duration
    Added a new DurationAdapter to handle Java's Duration type. This includes registering the adapter in the Serializer and implementing serialization to and deserialization from LongTags. This ensures seamless handling of Duration objects in the NBT system.

---
+++ nbt/src/main/java/core/nbt/serialization/adapter/DurationAdapter.java
@@ -0,0 +1,27 @@
+
+package core.nbt.serialization.adapter;
+
+import core.nbt.serialization.ParserException;
+import core.nbt.serialization.TagAdapter;
+import core.nbt.serialization.TagDeserializationContext;
+import core.nbt.serialization.TagSerializationContext;
+import core.nbt.tag.LongTag;
+import core.nbt.tag.Tag;
+import org.jspecify.annotations.NullMarked;
+
+import java.time.Duration;
+
+@NullMarked
+public class DurationAdapter implements TagAdapter<Duration> {
+    public static final DurationAdapter INSTANCE = new DurationAdapter();
+
+    @Override
+    public Duration deserialize(Tag tag, TagDeserializationContext context) throws ParserException {
+        return Duration.ofMillis(tag.getAsLong());
+    }
+
+    @Override
+    public Tag serialize(Duration duration, TagSerializationContext context) throws ParserException {
+        return new LongTag(duration.toMillis());
+    }
+}
