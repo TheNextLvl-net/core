7ae54f60716e32c35170382bbeaa298b2b4e64ec
david <github@nonswag.dev>
2025-04-27 14:35:39 +0200

    Added Javadocs to `ResourceMigrationException` constructors
    Documented constructors to clarify usage and context.

--- i18n/src/main/java/core/i18n/file/ResourceMigrationException.java
+++ i18n/src/main/java/core/i18n/file/ResourceMigrationException.java
@@ -1,10 +1,25 @@
 package core.i18n.file;
 
 public class ResourceMigrationException extends RuntimeException {
+    /**
+     * Constructs a new ResourceMigrationException with the specified detail message.
+     * <p>
+     * This exception is specifically used to indicate issues encountered during resource migration.
+     *
+     * @param message the detail message providing additional context about the exception.
+     */
     public ResourceMigrationException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new ResourceMigrationException with the specified detail message and cause.
+     * <p>
+     * This exception is specifically used to indicate issues encountered during resource migration.
+     *
+     * @param message the detail message providing additional context about the exception.
+     * @param cause   the cause of the exception, which can be used to retrieve details about the root issue.
+     */
     public ResourceMigrationException(String message, Throwable cause) {
         super(message, cause);
     }
