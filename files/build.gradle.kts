835c834dc248d7947d06ce31bdcee2a680e76bdb
david <github@nonswag.dev>
2025-04-28 22:19:58 +0200

    [ci skip] Bumped version to 3.0.0
    Updated project version from pre-release to stable release.

--- files/build.gradle.kts
+++ files/build.gradle.kts
@@ -15,7 +15,7 @@ tasks.compileJava {
 }
 
 group = "net.thenextlvl.core"
-version = "3.0.0-pre2"
+version = "3.0.0"
 
 repositories {
     mavenCentral()
--- i18n/build.gradle.kts
+++ i18n/build.gradle.kts
@@ -15,7 +15,7 @@ tasks.compileJava {
 }
 
 group = "net.thenextlvl.core"
-version = "3.0.0-pre4"
+version = "3.0.0"
 
 repositories {
     mavenCentral()
