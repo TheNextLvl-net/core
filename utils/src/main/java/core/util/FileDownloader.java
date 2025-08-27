55328a577619f9103e7d03dc072c69301d443023
david <github@nonswag.dev>
2025-08-27 20:12:23 +0200

    [ci skip] Added `@since 1.0.0` annotation in `FileDownloader`
    Indicated version introduction in Javadoc for better documentation.

--- utils/src/main/java/core/util/FileDownloader.java
+++ utils/src/main/java/core/util/FileDownloader.java
@@ -14,11 +14,12 @@ import java.nio.file.Path;
  *
  * @param url         the url to download the file from
  * @param destination the destination to save the file to
+ * @since 1.0.0
  */
 @NullMarked
 public record FileDownloader(URL url, Path destination) {
     /**
-     * Download a file from an url to the given path
+     * Download a file from a URL to the given path
      *
      * @throws IOException thrown if something goes wrong
      */
