27aeb9db371432e3ed7801fdd0d2009401786c92
david <github@nonswag.dev>
2025-08-27 20:13:00 +0200

    Simplified `FileDownloader.download` method
    Replaced `URLConnection` and `InputStream` with inline resource management to make the code more concise and readable.

--- utils/src/main/java/core/util/FileDownloader.java
+++ utils/src/main/java/core/util/FileDownloader.java
@@ -3,9 +3,7 @@ package core.util;
 import org.jspecify.annotations.NullMarked;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -24,9 +22,8 @@ public record FileDownloader(URL url, Path destination) {
      * @throws IOException thrown if something goes wrong
      */
     public void download() throws IOException {
-        URLConnection urlConnection = url.openConnection();
-        try (InputStream inputStream = urlConnection.getInputStream()) {
-            Files.write(destination(), inputStream.readAllBytes());
+        try (var input = url.openConnection().getInputStream()) {
+            Files.write(destination(), input.readAllBytes());
         }
     }
 }
