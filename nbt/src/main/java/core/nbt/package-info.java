0622bf7e8cc470804cdb24fc584a87c80d85cc99
david <github@nonswag.dev>
2025-09-05 16:58:26 +0200

    Marked `core.nbt` package as @Deprecated
    Annotated `core.nbt` with `@Deprecated(since = "2.4.0")` to indicate its planned migration to a new artifact and namespace.

---
+++ nbt/src/main/java/core/nbt/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This library will be moved to a separate repository.
+ * <p>
+ * The package will be moved from {@code core.nbt} to {@code net.thenextlvl.nbt}<br/>
+ * The project group will be changed from {@code net.thenextlvl.core} to {@code net.thenextlvl}<br/>
+ * The project name will remain the same.
+ *
+ * @deprecated Move to the new artifact id {@code net.thenextlvl:nbt:VERSION}.
+ */
+@Deprecated(since = "2.4.0")
+package core.nbt;
+
