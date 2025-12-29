81dfdca44131ab0ed2850c5b0fdf2e1aab51e0d7
david <mail@nonswag.dev>
2025-12-29 21:23:21 +0100

    Remove unnecessary suppression in ComponentCommandExceptionType

--- paper/src/main/java/core/paper/brigadier/exceptions/ComponentCommandExceptionType.java
+++ paper/src/main/java/core/paper/brigadier/exceptions/ComponentCommandExceptionType.java
@@ -8,13 +8,11 @@ import net.kyori.adventure.text.Component;
  * Represents a custom exception type for component-based command errors.
  */
 public final class ComponentCommandExceptionType extends SimpleCommandExceptionType {
-
     /**
      * Represents a custom exception type for component-based command errors.
      *
      * @param component The component describing the exception.
      */
-    @SuppressWarnings("UnstableApiUsage")
     public ComponentCommandExceptionType(Component component) {
         super(MessageComponentSerializer.message().serialize(component));
     }
